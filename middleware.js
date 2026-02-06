e6b2dd74d21f82422f6ba96bf292fb0e92ab3c7f
tamgar <tamtam113.tg@gmail.com>
2026-02-06 09:46:05 -0500

    new improved

---
+++ middleware.js
@@ -0,0 +1,9 @@
+export function logger(req, res, next) {
+  const time = new Date().toISOString();
+  console.log(`[${time}] ${req.method} ${req.originalUrl}`);
+  next();
+}
+
+export function errorHandler(err, req, res, next) {
+  console.error("Error:", err.message);
+  res.status(500).json({ error: err.message });
--- middlewares.js
+++
@@ -1,8 +0,0 @@
-export function logReq(req, res, next) {
-  console.log(req.method, req.url);
-  next();
-}
-
-export function globalErr(err, req, res, next) {
-  res.status(err.status || 500).json({ error: err.message });
-}
